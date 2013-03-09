ea40f4f6dcb7d844618b3752a40db2d4d3c828a1
Michael Bestas <mikeioannina@gmail.com>
2013-03-09 05:33:10 +0200

    Build kernel from source
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I59ddd17073cc3f80bfd48e2139b7a0f68362c8d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,9 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/smultron/BoardConfigVendor.mk
 
+TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
+TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
+
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := false
 SENSORS_ACCEL_BMA250_INPUT := true
@@ -8,4 +11,3 @@ SENSORS_PROXIMITY_APDS970X := true
 SENSORS_PRESSURE_BMP180 := false
 
 TARGET_OTA_ASSERT_DEVICE := ST15i,ST15a,smultron
-
